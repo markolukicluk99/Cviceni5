6b3864dcf3f781afe604b31d1821a06fefd4a927
Marko Lukic <markolukicluk99@gmail.com>
2019-10-23 16:13:58 +0200

    Funkce Tisk + Soucet

--- Cviceni5/Cviceni5.cpp
+++ Cviceni5/Cviceni5.cpp
@@ -2,18 +2,37 @@
 
 #include <iostream>
 
+
 void soucet(double a[2], double b[2], double c[2])
 {
+	//c[0] = a[0] + b[0];
 
+	c[0] = printf("%f + %fj", a[0], b[0]);
+
+	//c[1] = a[1] + b[1];
 }
 
-void tisk(double c[2]) {
-
-
-}
-
-int main()
+void rozdil(double a[2], double b[2], double c[2])
 {
-    std::cout << "Hello World!\n";
+	//c[0] = a[0] - b[0];
+	//c[1] = a[1] - b[1];
 }
 
+void tisk(double c[2]) 
+{
+	printf("%c\n", c[0]);
+}
+
+int main(void)
+{
+	double a[2] = { 1.5, 2 };
+	double b[2] = { 4.5, -2 };
+	double c[2];
+
+	soucet(a, b, c);
+	rozdil(a, b, c);
+	tisk(c);
+
+	return 0;
+
+}
